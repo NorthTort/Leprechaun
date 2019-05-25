c345f3af945aedc9d42061abf2ca67aa6417a1fd
NordTort <41715818+NorthTort@users.noreply.github.com>
2019-05-25 15:23:17 +0300

    Create Leprechaun.js

---
+++ Leprechaun.js
@@ -0,0 +1,33 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+var prefix = '+'
+ 
+ client.on('message', message => {
+    if(message.author === client.user) return;
+    
+	 if(message.content.startsWith(prefix + 'say')) {
+  if(message.author.id !== '386834372466049024') return;
+    const args = message.content.slice(prefix.length).split('say'); 
+	  message.delete()
+  .then(msg => console.log(`Deleted message from ${msg.author.username}`))
+  .catch(console.error); 
+     		message.channel.sendMessage(message.content.slice(prefix.length).split('say'));
+
+	 }
+  	 if(message.content.startsWith(prefix + 'send')) {
+  if(message.author.id !== '435498881040777231') return;
+    const args = message.content.slice(prefix.length).split('send'); 
+	  message.delete()
+  .then(msg => console.log(`Deleted message from ${msg.author.username}`))
+  .catch(console.error); 
+     		message.channel.sendMessage(message.content.slice(prefix.length).split('send'));
+	 }
+ });
+
+client.login(process.env.BOT_TOKEN);
